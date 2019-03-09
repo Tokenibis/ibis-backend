705862261865fb9d2d8ed23e1c722900c8078def
Thien-Nam <thiennam.tnd@gmail.com>
2019-03-09 13:26:09 -0700

    initial model implementation

--- api/ibis/admin.py
+++ api/ibis/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Transaction
 
-admin.site.register(Transaction)
+import ibis.models as models
+
+admin.site.register(models.Follow)
+admin.site.register(models.Exchange)
+admin.site.register(models.Post)
+admin.site.register(models.Transaction)
+admin.site.register(models.Article)
+admin.site.register(models.Event)
+admin.site.register(models.Comment)
+admin.site.register(models.Upvote)
+admin.site.register(models.Downvote)
+admin.site.register(models.RSVP)
--- api/ibis/models.py
+++ api/ibis/models.py
@@ -1,19 +1,131 @@
-from datetime import datetime
-
 from django.db import models
 from profiles.models import Profile
 
+CAT_MAX_LEN = 10
+TX_MAX_LEN = 160
+TITLE_MAX_LEN = 50
+DESC_MAX_LEN = 320
 
-class Transaction(models.Model):
 
-    sender = models.ForeignKey(
-        Profile, related_name='sender', on_delete=models.CASCADE)
-    receiver = models.ForeignKey(
-        Profile, related_name='receiver', on_delete=models.CASCADE)
-
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
-    datetime = models.DateTimeField(default=datetime.now)
+class Nonprofit(models.Model):
+    id = models.OneToOneField(
+        Profile,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    category = models.CharField(max_length=CAT_MAX_LEN)
+    link = models.TextField()
     description = models.TextField()
 
-    def __str__(self):
-        return '{} - {} - {}'.format(self.sender, self.receiver, self.datetime)
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    target = models.OneToOneField(
+        Profile,
+        related_name='follower',
+        on_delete=models.CASCADE,
+    )
+
+
+class Exchange(models.Model):
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    is_deposit = models.BooleanField()
+    amount = models.PositiveIntegerField()
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+
+
+class Transaction(models.Model):
+    id = models.OneToOneField(
+        Post,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    target = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+
+    amount = models.PositiveIntegerField()
+    description = models.CharField(max_length=TX_MAX_LEN)
+
+
+class Article(models.Model):
+    id = models.OneToOneField(
+        Post,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    title = models.CharField(max_length=TITLE_MAX_LEN)
+
+    description = models.CharField(max_length=DESC_MAX_LEN)
+
+    content = ''  # TODO: how to handle this?
+
+
+class Event(models.Model):
+    id = models.OneToOneField(
+        Post,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    title = models.CharField(max_length=TITLE_MAX_LEN)
+    link = models.TextField()
+    description = models.CharField(max_length=DESC_MAX_LEN)
+
+
+class Comment(models.Model):
+    id = models.OneToOneField(
+        Post,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    parent = models.ForeignKey(
+        Post,
+        related_name='child',
+        on_delete=models.CASCADE,
+    )
+
+
+class Upvote(models.Model):
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+    )
+
+
+class Downvote(models.Model):
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    comment = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE,
+    )
+
+
+class RSVP(models.Model):
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+    )
