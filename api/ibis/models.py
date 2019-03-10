234c54d4c77b69b2d513034e03064ae89b80eb9d
Thien-Nam <thiennam.tnd@gmail.com>
2019-03-09 20:08:09 -0700

    update ibis model components to include timestamp/softdelete/status

--- api/ibis/models.py
+++ api/ibis/models.py
@@ -1,24 +1,70 @@
 from django.db import models
+from model_utils import Choices
+from model_utils.models import TimeStampedModel
+from model_utils.models import StatusModel, SoftDeletableModel
 from profiles.models import Profile
 
+# TODO: best way to handle this? maybe in settings?
 CAT_MAX_LEN = 10
-TX_MAX_LEN = 160
 TITLE_MAX_LEN = 50
+TX_MAX_LEN = 160
 DESC_MAX_LEN = 320
 
 
-class Nonprofit(models.Model):
-    id = models.OneToOneField(
+class Nonprofit(TimeStampedModel, SoftDeletableModel, StatusModel):
+    user = models.OneToOneField(
         Profile,
         on_delete=models.CASCADE,
         primary_key=True,
     )
-    category = models.CharField(max_length=CAT_MAX_LEN)
-    link = models.TextField()
+
+    CATEGORY = Choices(
+        'Animal Rights, Welfare, and Services',
+        'Wildlife Conservation',
+        'Zoos and Aquariums',
+        'Libraries, Historical Societies and Landmark Preservation',
+        'Museums',
+        'Performing Arts',
+        'Public Broadcasting and Media',
+        'United Ways',
+        'Jewish Federations',
+        'Housing and Neighborhood Development',
+        'Early Childhood Programs and Services',
+        'Youth Education Programs and Services',
+        'Adult Education Programs and Services',
+        'Special Education',
+        'Education Policy and Reform',
+        'Scholarship and Financial Support',
+        'Environmental Protection and Conservation',
+        'Botanical Gardens, Parks, and Nature Centers',
+        'Diseases, Disorders, and Disciplines',
+        'Patient and Family Support',
+        'Treatment and Prevention Services',
+        'Medical Research',
+        'Advocacy and Education',
+        'Children\'s and Family Services',
+        'Youth Development, Shelter, and Crisis Services',
+        'Food Banks, Food Pantries, and Food Distribution',
+        'Multipurpose Human Service Organizations',
+        'Homeless Services',
+        'Social Services',
+        'Development and Relief Services',
+        'International Peace, Security, and Affairs',
+        'Humanitarian Relief Supplies',
+        'Non-Medical Science & Technology Research',
+        'Social and Public Policy Research',
+        'Religious Activities',
+        'Religious Media and Broadcasting',
+        'Other',
+    )
+
+    category = models.CharField(choices=CATEGORY, max_length=TITLE_MAX_LEN)
     description = models.TextField()
 
+    STATUS = Choices('pending', 'approved')
 
-class Follow(models.Model):
+
+class Follow(TimeStampedModel):
     user = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
@@ -30,7 +76,7 @@ class Follow(models.Model):
     )
 
 
-class Exchange(models.Model):
+class Exchange(TimeStampedModel):
     user = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
@@ -39,15 +85,15 @@ class Exchange(models.Model):
     amount = models.PositiveIntegerField()
 
 
-class Post(models.Model):
+class Post(TimeStampedModel, SoftDeletableModel):
     user = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
     )
 
 
-class Transaction(models.Model):
-    id = models.OneToOneField(
+class Transaction(StatusModel):
+    post = models.OneToOneField(
         Post,
         on_delete=models.CASCADE,
         primary_key=True,
@@ -60,22 +106,24 @@ class Transaction(models.Model):
     amount = models.PositiveIntegerField()
     description = models.CharField(max_length=TX_MAX_LEN)
 
+    STATUS = Choices('requested', 'completed')
 
-class Article(models.Model):
-    id = models.OneToOneField(
+
+class Article(StatusModel):
+    post = models.OneToOneField(
         Post,
         on_delete=models.CASCADE,
         primary_key=True,
     )
     title = models.CharField(max_length=TITLE_MAX_LEN)
-
     description = models.CharField(max_length=DESC_MAX_LEN)
+    content = models.FileField()
 
-    content = ''  # TODO: how to handle this?
+    STATUS = Choices('draft', 'published')
 
 
-class Event(models.Model):
-    id = models.OneToOneField(
+class Event(StatusModel):
+    post = models.OneToOneField(
         Post,
         on_delete=models.CASCADE,
         primary_key=True,
@@ -84,9 +132,11 @@ class Event(models.Model):
     link = models.TextField()
     description = models.CharField(max_length=DESC_MAX_LEN)
 
+    STATUS = Choices('draft', 'published')
+
 
 class Comment(models.Model):
-    id = models.OneToOneField(
+    post = models.OneToOneField(
         Post,
         on_delete=models.CASCADE,
         primary_key=True,
@@ -98,7 +148,7 @@ class Comment(models.Model):
     )
 
 
-class Upvote(models.Model):
+class Upvote(TimeStampedModel):
     user = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
@@ -109,7 +159,7 @@ class Upvote(models.Model):
     )
 
 
-class Downvote(models.Model):
+class Downvote(TimeStampedModel):
     user = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
@@ -120,7 +170,7 @@ class Downvote(models.Model):
     )
 
 
-class RSVP(models.Model):
+class RSVP(TimeStampedModel):
     user = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
