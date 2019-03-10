3ddd9b9c11749a5a6721a8780adf268dd96bec75
Thien-Nam <thiennam.tnd@gmail.com>
2019-03-09 22:49:24 -0700

    provide many-to-many model and field for nonprofit categorization

--- api/ibis/admin.py
+++ api/ibis/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 import ibis.models as models
 
+admin.site.register(models.NonprofitCategory)
 admin.site.register(models.Nonprofit)
 admin.site.register(models.Follow)
 admin.site.register(models.Exchange)
--- api/ibis/models.py
+++ api/ibis/models.py
@@ -11,54 +11,23 @@ TX_MAX_LEN = 160
 DESC_MAX_LEN = 320
 
 
-class Nonprofit(TimeStampedModel, SoftDeletableModel, StatusModel):
+class NonprofitCategory(models.Model):
+    title = models.CharField(max_length=TITLE_MAX_LEN)
+    description = models.CharField(max_length=DESC_MAX_LEN)
+
     user = models.OneToOneField(
         Profile,
         on_delete=models.CASCADE,
         primary_key=True,
     )
 
-    CATEGORY = Choices(
-        'Animal Rights, Welfare, and Services',
-        'Wildlife Conservation',
-        'Zoos and Aquariums',
-        'Libraries, Historical Societies and Landmark Preservation',
-        'Museums',
-        'Performing Arts',
-        'Public Broadcasting and Media',
-        'United Ways',
-        'Jewish Federations',
-        'Housing and Neighborhood Development',
-        'Early Childhood Programs and Services',
-        'Youth Education Programs and Services',
-        'Adult Education Programs and Services',
-        'Special Education',
-        'Education Policy and Reform',
-        'Scholarship and Financial Support',
-        'Environmental Protection and Conservation',
-        'Botanical Gardens, Parks, and Nature Centers',
-        'Diseases, Disorders, and Disciplines',
-        'Patient and Family Support',
-        'Treatment and Prevention Services',
-        'Medical Research',
-        'Advocacy and Education',
-        'Children\'s and Family Services',
-        'Youth Development, Shelter, and Crisis Services',
-        'Food Banks, Food Pantries, and Food Distribution',
-        'Multipurpose Human Service Organizations',
-        'Homeless Services',
-        'Social Services',
-        'Development and Relief Services',
-        'International Peace, Security, and Affairs',
-        'Humanitarian Relief Supplies',
-        'Non-Medical Science & Technology Research',
-        'Social and Public Policy Research',
-        'Religious Activities',
-        'Religious Media and Broadcasting',
-        'Other',
+class Nonprofit(TimeStampedModel, SoftDeletableModel, StatusModel):
+    user = models.OneToOneField(
+        Profile,
+        on_delete=models.CASCADE,
+        primary_key=True,
     )
-
-    category = models.CharField(choices=CATEGORY, max_length=TITLE_MAX_LEN)
+    category = models.ManyToManyField(NonprofitCategory)
     description = models.TextField()
 
     STATUS = Choices('pending', 'approved')
