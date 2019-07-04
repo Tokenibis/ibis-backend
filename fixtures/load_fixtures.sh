45c9ecf10d27d6a0cc3915bbc4d4a9c488ea5d69
Thien-Nam <thiennam.tnd@gmail.com>
2019-07-04 10:53:09 -0600

    add script to clean and load fixtures for development

---
+++ fixtures/load_fixtures.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# !!WARNING!! This script deletes all existing data in the db.sqlite
+# database and loads it with the latest fake data.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+./make_fixtures.py
+
+cd $DIR/../api
+rm db.sqlite3
+rm ibis/migrations/ -rf
+rm users/migrations -rf
+python3 manage.py makemigrations users
+python3 manage.py makemigrations ibis
+python3 manage.py migrate users
+python3 manage.py migrate ibis
+python3 manage.py migrate
+python3 manage.py createsuperuser --username admin --email 'admin@admin.com' --noinput
+python3 manage.py loaddata $DIR/fixtures.json && python3 manage.py runserver
