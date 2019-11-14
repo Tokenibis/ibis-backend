fbb0070f272b682f81bf96bea7a88d997fe1bb4d
Thien-Nam Dinh <tdinh@ip-172-31-22-153.us-east-2.compute.internal>
2019-11-14 04:10:07 +0000

    make users reauthenticate when logging in

--- api/users/views.py
+++ api/users/views.py
@@ -64,7 +64,7 @@ class Login(APIView):
         view.request = request
         view.adapter = adapter
         client = view.get_client(request, app)
-        action = AuthAction.AUTHENTICATE
+        action = AuthAction.REAUTHENTICATE
         auth_params = provider.get_auth_params(request, action)
         client.state = SocialLogin.stash_state(request)
         url = client.get_redirect_url(adapter.authorize_url, auth_params)
