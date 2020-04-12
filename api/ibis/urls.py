2c5842ff123c1a488d872dd1269bac707f43a418
Thien-Nam <thiennam.tnd@gmail.com>
2020-04-12 15:56:16 -0600

    re-add ibis/urls.py that got accidently deleted in the last commit

---
+++ api/ibis/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+import ibis.views as views
+
+urlpatterns = [
+    path('price/', views.PriceView.as_view(), name='quote'),
+    path('quote/', views.QuoteView.as_view(), name='quote'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('login-anon/', views.AnonymousLoginView.as_view(), name='login-anon'),
+    path('login-pass/', views.PasswordLoginView.as_view(), name='login-pass'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('identify/', views.IdentifyView.as_view(), name='identify'),
+    path('payment/', views.PaymentView.as_view(), name='payment'),
+]
