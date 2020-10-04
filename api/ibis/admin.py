24971c2307a10c911b6d1fcc28f86d7d4f392d70
Thien-Nam Dinh <tdinh@ip-172-31-31-33.us-east-2.compute.internal>
2020-10-04 16:07:04 +0000

    get rid of failed attempt to streamline user password admin

--- api/ibis/admin.py
+++ api/ibis/admin.py
@@ -15,29 +15,31 @@ from django.contrib.auth.admin import UserAdmin
 #         return '${:.2f}'.format(obj.fundraised() / 100)
 
 
-@admin.register(models.Person)
-class PersonAdmin(UserAdmin):
-    readonly_fields = ('balance_str', 'donated_str')
+# @admin.register(models.Person)
+# class PersonAdmin(UserAdmin):
+#     readonly_fields = ('balance_str', 'donated_str')
 
-    def balance_str(self, obj):
-        return '${:.2f}'.format(obj.balance() / 100)
+#     def balance_str(self, obj):
+#         return '${:.2f}'.format(obj.balance() / 100)
 
-    def donated_str(self, obj):
-        return '${:.2f}'.format(obj.donated() / 100)
+#     def donated_str(self, obj):
+#         return '${:.2f}'.format(obj.donated() / 100)
 
 
-@admin.register(models.Bot)
-class BotAdmin(UserAdmin):
-    readonly_fields = ('balance_str', 'rewarded_str')
+# @admin.register(models.Bot)
+# class BotAdmin(UserAdmin):
+#     readonly_fields = ('balance_str', 'rewarded_str')
 
-    def balance_str(self, obj):
-        return '${:.2f}'.format(obj.balance() / 100)
+#     def balance_str(self, obj):
+#         return '${:.2f}'.format(obj.balance() / 100)
 
-    def rewarded_str(self, obj):
-        return '${:.2f}'.format(obj.rewarded() / 100)
+#     def rewarded_str(self, obj):
+#         return '${:.2f}'.format(obj.rewarded() / 100)
 
 
 admin.site.register(models.Organization)
+admin.site.register(models.Person)
+admin.site.register(models.Bot)
 admin.site.register(models.OrganizationCategory)
 admin.site.register(models.ExchangeCategory)
 admin.site.register(models.News)
