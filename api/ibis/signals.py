08f21784e124776af2fac7031b55e061de7ee9ab
Thien-Nam Dinh <tdinh@ip-172-31-31-33.us-east-2.compute.internal>
2020-10-05 18:26:05 +0000

    initialize user with their date joined timestamp

--- api/ibis/signals.py
+++ api/ibis/signals.py
@@ -13,3 +13,13 @@ def updatePersonScore(sender, instance, created, **kwargs):
     user = instance.user
     user.score = instance.created.timestamp()
     user.save()
+
+
+@receiver(post_save, sender=models.Person)
+def initializePersonScore(sender, instance, created, raw, **kwargs):
+    if raw or not created:
+        return
+
+    # user score is defined as the unix time of their latest donation
+    instance.score = instance.date_joined.timestamp()
+    instance.save()
