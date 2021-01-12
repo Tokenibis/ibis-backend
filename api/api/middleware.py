9aa35de74c88bec544762e39ec6f2d6e5a38b934
Thien-Nam Dinh <tdinh@ip-172-31-23-192.us-east-2.compute.internal>
2021-01-12 05:09:41 +0000

    update authentication testing middleware

--- api/api/middleware.py
+++ api/api/middleware.py
@@ -6,11 +6,14 @@ import ibis.models
 
 def AuthenticateAllMiddleware(get_response):
     def middleware(request):
-        if settings.AUTHENTICATE_AS == 'person':
+        if ibis.models.User.objects.filter(username=settings.AUTHENTICATE_AS):
+            user = ibis.models.User.objects.get(
+                username=settings.AUTHENTICATE_AS)
+        elif settings.AUTHENTICATE_AS == '__person__':
             user = ibis.models.Person.objects.last()
-        elif settings.AUTHENTICATE_AS == 'organization':
+        elif settings.AUTHENTICATE_AS == '__organization__':
             user = ibis.models.Organization.objects.last()
-        elif settings.AUTHENTICATE_AS == 'bot':
+        elif settings.AUTHENTICATE_AS == '__bot__':
             user = ibis.models.Bot.objects.last()
         else:
             raise ValueError('Unknown test authentication type')
